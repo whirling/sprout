782191e82ed9b9913f8d9e1cc57ecb0db100598b
NOSH2000 <nowshins0363@hillcresths.org>
2017-08-17 11:03:51 -0400

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Resource
 
 # Create your views here.
 def resource_list(request):
-    return render(request, 'blog/resource_list.html', {})
+    resources = Resource.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/resource_list.html', {'resources': resources})
