81a935cf3be8e2f4d299ca7f29f416333301bf6e
NOSH2000 <nowshins0363@hillcresths.org>
2017-08-23 09:56:04 -0400

    Help page

--- blog/views.py
+++ blog/views.py
@@ -67,19 +67,19 @@ def help(request):
     return render(request, 'blog/help.html', {})
 
 def hotline(request):
-    resources = Resource.objects.filter(vent=True).order_by('title')
+    resources = Resource.objects.filter(hotline=True).order_by('title')
     return render(request, 'blog/hotline.html', {"resources":resources})
 
 def therapy(request):
-    resources = Resource.objects.filter(vent=True).order_by('title')
+    resources = Resource.objects.filter(therapy=True).order_by('title')
     return render(request, 'blog/therapy.html', {"resources":resources})
 
 def self_help(request):
-    resources = Resource.objects.filter(vent=True).order_by('title')
+    resources = Resource.objects.filter(self_help=True).order_by('title')
     return render(request, 'blog/self_help.html', {"resources":resources})
 
 def chat(request):
-    resources = Resource.objects.filter(vent=True).order_by('title')
+    resources = Resource.objects.filter(chat=True).order_by('title')
     return render(request, 'blog/chat.html', {"resources":resources})
 
 ##################################################################
